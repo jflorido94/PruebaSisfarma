540b6e2d9622d5e67e06184afde7c7f5866efe13
jflorido94 <hbranky22@hotmail.com>
2022-06-08 17:36:43 +0200

    sections with includes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,9 +35,9 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/','User::index');
 
-$routes->get('/holamundo', 'HolaMundo::index');
+$routes->get('/login', 'User::index');
 
 /*
  * --------------------------------------------------------------------
---
+++ app/Views/login.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('title') ?>
+  Login prueba
+<?= $this->endSection() ?>
